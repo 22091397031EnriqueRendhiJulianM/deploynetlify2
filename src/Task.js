8fff6d51b420b200cf19708aff5d592c5a4d82ed
22091397031EnriqueRendhiJulianM <enrique.22031@mhs.unesa.ac.id>
2024-10-27 02:28:22 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Enrique Rendhi Julian Mahardhika" // TODO: replace this
+const studentId = "FS10420569" // TODO: replace this
+
+export { url, studentName, studentId }
